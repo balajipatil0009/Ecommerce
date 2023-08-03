69efb8ce0069285e4e0547a2c505466081fe5b33
Shubham <shubhamdesai20003@gmail.com>
2023-08-03 19:05:27 +0530

    Login Part-II

--- src/componunts/Login.jsx
+++ src/componunts/Login.jsx
@@ -1,13 +1,21 @@
 const Login = ()=>{
     return (
-        <div className="flex bg-white absolute z-10 w-1/2 h-1/2 top-1/4 left-1/4 border-2">
-            <div className="border bg-[url('Login.png')] bg-cover bg-center bg-no-repeat text-left p-10 flex flex-col w-1/3 ">
+        <div className="flex bg-white absolute z-10 w-[700px] h-[550px] top-1/4 left-1/4 border-2">
+            <div className=" bg-[url('Login.png')] bg-contain bg-no-repeat text-left p-10 flex flex-col w-[300px] ">
                 <span className="text-4xl font-semibold text-white">Login</span>
                 <span className="text-slate-200 text-md mt-4">Get access to your <br /> Orders,  Wishlist and <br /> Recommendations</span>
             </div>
 
-            <div className="">
+            <div className="flex flex-col p-8 gap-4 ">
 
+                <input className="border-none" type="text" placeholder="Enter Email/Mobile Number" />
+                <hr className="border -mt-2.5"/>
+
+                <span className="text-left mt-8 text-sm text-slate-400">By continuing, you agree to Flipkart's <span className="text-blue-600">Terms of Use</span> and <span className="text-blue-600">Privacy Policy.</span> </span>
+
+                <a href="#" className="border-2 text-white font-medium bg-orange-500 p-3 text-center w-full m-0">Request OTP</a>
+
+                <a href="#" className="absolute bottom-8 left-1/2 text-blue-600">New to Flipkart? Create an account</a>
             </div>
 
         </div>
